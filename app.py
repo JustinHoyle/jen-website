ef55de4c5b59c00e1b1a89c600589de2d425b833
JustinHoyle <54443720+JustinHoyle@users.noreply.github.com>
2022-09-28 11:37:03 -0400

    Working Site with Flask/HTML
    To fix errors, move images to static folder, css to static/css, and all html to templates

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/Home.html')
+def Home():
+  return render_template('Home.html')
 
+@app.route('/About.html')
+def About():
+  return render_template('About.html')
+
+@app.route('/Contact.html')
+def Contact():
+  return render_template('Contact.html')
+
+@app.route('/')
 def index():
-
-  return "<html><body><h1>Test site running under Flask</h1></body></html>"
+  return redirect('Home.html')
 
 if __name__ == "__main__":
 
