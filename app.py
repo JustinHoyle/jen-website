453ab3a6e9f32890165c35e632a6c1493895cc5a
JustinHoyle <54443720+JustinHoyle@users.noreply.github.com>
2022-10-03 19:26:40 -0400

    404 added

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ def Contact():
 def index():
   return redirect('Home.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
 
   app.run(host='0.0.0.0',debug=True)
